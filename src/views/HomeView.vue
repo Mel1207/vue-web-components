ca6e7b0dd21727c297091e1b7681cd2a230c209f
Mel <meljohnpualon@gmail.com>
2022-07-21 19:26:04 +0800

    added commit testing for dev branch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <h1>hey this is Home</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis recusandae ullam illum error molestias omnis doloribus iure animi! Optio, ducimus?</p>
     <p>test commit dev branch</p>
+    <p style="font-weight: bold; font-size: 50px; color: gray;">test 2nd commit for dev branch</p>
   </div>
 </template>
 
