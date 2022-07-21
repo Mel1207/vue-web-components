ac4d019aba979e2b053831d6de4e825e3092987e
Mel <meljohnpualon@gmail.com>
2022-07-21 18:58:22 +0800

    test commit for dev branch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h1>hey this is Home</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis recusandae ullam illum error molestias omnis doloribus iure animi! Optio, ducimus?</p>
+    <p>test commit dev branch</p>
   </div>
 </template>
 
