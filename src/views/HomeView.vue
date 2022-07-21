ecc94b6f1ad6b1978517ef34e41af1a3ea929bf8
Mel <meljohnpualon@gmail.com>
2022-07-21 18:45:18 +0800

    added text for testing push for dev branch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>hey this is Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis recusandae ullam illum error molestias omnis doloribus iure animi! Optio, ducimus?</p>
   </div>
 </template>
 
