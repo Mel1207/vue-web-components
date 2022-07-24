142ecf2c7da3c94472a78fc05b68bd050778f284
Mel <meljohnpualon@gmail.com>
2022-07-24 22:45:49 +0800

    added new changes from code

---
+++ src/components/Navbar/NavLinks.vue
@@ -0,0 +1,16 @@
+<template>
+    <router-link to=""><p>Home</p></router-link>
+    <router-link to=""><p>About</p></router-link>
+    <router-link to=""><p>Home</p></router-link>
+    <router-link to=""><p>Home</p></router-link>
+</template>
+
+<script>
+export default {
+    props: []
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="navbar">
+        <div class="nav-logo">
+            <img src="../../assets/images/logo-main.svg" alt="">
+        </div>
+        
+
+        <ul class="nav-menu">
+            <li><a href="#!">Home</a></li>
+            <li><a href="#!">About</a></li>
+            <li><a href="#!">Blogs</a></li>
+            <li><a href="#!">Contacts</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        const navLinks = [
+        {
+            id: 1,
+            item: 'Home',
+            link: '/'
+        },
+        {
+            id: 2,
+            item: 'About',
+            link: '/About'
+        },
+        {
+            id: 3,
+            item: 'Blogs',
+            link: '/Blogs'
+        },
+        {
+            id: 4,
+            item: 'Contacts',
+            link: '/Contacts'
+        },
+    ]
+
+        return {
+            navLinks
+        }
+    }
+}
+</script>
