5e1051f156d48d80a5cb462055570cd5e942fd28
Mel <meljohnpualon@gmail.com>
2022-07-24 22:52:13 +0800

    added home changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,21 @@
   <Navbar />
   <div class="home">
     <h1>hey this is Home</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis recusandae ullam illum error molestias omnis doloribus iure animi! Optio, ducimus?</p>
-    <p>test commit dev branch</p>
-    <p style="font-weight: bold; font-size: 50px; color: gray;">test 2nd commit for dev branch</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis recusandae ullam illum error</p>
   </div>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue'
+import Navbar from '../components/Navbar/Navbar.vue'
 
 export default {
   name: 'HomeView',
-  components: { Navbar }
+  components: { Navbar },
+  setup() {
+    console.log('hey mel')
+
+    return {
+    }
+  }
 }
 </script>
