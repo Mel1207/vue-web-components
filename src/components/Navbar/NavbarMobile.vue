9467dd2e911e6e72091acfed5eea53e9e6f2c3f1
Mel <meljohnpualon@gmail.com>
2022-07-29 06:57:03 +0800

    added mobile menu component

--- src/components/Navbar/NavbarMobile.vue
+++ src/components/Navbar/NavbarMobile.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="navbar-mobile-menu">
-        <button @click="closeNav">Close me</button>
+        <button @click="closeNav">
+            <img src="../../assets/images/icon-close.svg" alt="close">
+        </button>
+
+        <ul>
+            <li v-for="link in navLinks" :key="link.id">{{ link.item }}</li>
+        </ul>
     </div>
 </template>
 
@@ -12,8 +18,32 @@ export default {
             context.emit('close')
         }
 
+        const navLinks = [
+            {
+                id: 1,
+                item: 'Home',
+                link: '/'
+            },
+            {
+                id: 2,
+                item: 'About',
+                link: 'about'
+            },
+            {
+                id: 3,
+                item: 'Blogs',
+                link: '/Blogs'
+            },
+            {
+                id: 4,
+                item: 'Contacts',
+                link: '/Contacts'
+            }
+        ]
+
         return {
-            closeNav
+            closeNav,
+            navLinks
         }
     }
 }
