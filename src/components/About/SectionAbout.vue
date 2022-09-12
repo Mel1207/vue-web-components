65091308bda976dc3ea3e543e17d6ef7bdfea051
Mel <meljohnpualon@gmail.com>
2022-09-13 03:53:43 +0800

    added flex layout for about section

--- src/components/About/SectionAbout.vue
+++ src/components/About/SectionAbout.vue
@@ -1,11 +1,22 @@
 <template>
-    <div class="container flex item-center">
-        <h2>Text about</h2>
+    <div class="container">
+        <div class="flex item-center">
+            <h2>Text about</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae voluptates nostrum tempora!</p>
+            <Button title="Learn More" type="btn-primary" />
+        </div>
+        <img src="" alt="">
     </div>
 </template>
 
 <script>
+import Button from '../Buttons/Button.vue' 
+
 export default {
+    props: ['title', 'type'],
+    components: { Button },
+    setup() {
+    }
 
 }
 </script>
