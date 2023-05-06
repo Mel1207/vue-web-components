398f305bd9cdd05622ba07bfa6f9ffa3af9c9f17
Mel <meljohnpualon@gmail.com>
2023-05-07 05:16:20 +0800

    added accordion component

---
+++ src/components/Accordion/Accordion.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-for="item in accItems" :key="item.id">
+    <div class="acc-title">{{ item.title }}</div>
+    <div class="acc-message">{{ item.message }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  setup() {
+    const accItems = [
+      { 
+        id: 1,
+        title: "Test title 1",
+        message: "lorem ipsum dolor sit amet is simply dummy text of the printing and typesetting industry.",
+        isOpen: true
+      },
+      { 
+        id: 2,
+        title: "Test title 2",
+        message: "lorem ipsum dolor sit amet is simply dummy text of the printing and typesetting industry.",
+        isOpen: false
+      },
+      { 
+        id: 3,
+        title: "Test title 3",
+        message: "lorem ipsum dolor sit amet is simply dummy text of the printing and typesetting industry.",
+        isOpen: false
+      }
+    ]
+
+    return { accItems }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Home/SectionFeature.vue
+++ src/components/Home/SectionFeature.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="container">
     <h1>test</h1>
+    <Accordion />
   </div>
 </template>
 
 <script>
-export default {
+import Accordion from '../Accordion/Accordion.vue'
 
+export default {
+  components: { Accordion },
+  setup() {
+
+
+    return {}
+  }
 }
 </script>
