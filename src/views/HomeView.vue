75735a803f54f26ac877292938906e06af6be79f
Mel <meljohnpualon@gmail.com>
2023-05-07 04:27:55 +0800

    added new section component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,19 @@
   <Navbar />
   <Header />
   <SectionAbout />
+  <SectionFeature />
 </template>
 
 <script>
 import Header from '../components/Header/Header.vue'
 import Navbar from '../components/Navbar/Navbar.vue'
-import SectionAbout from '../components/About/SectionAbout.vue'
+import SectionAbout from '../components/Home/SectionAbout.vue'
+import SectionFeature from '../components/Home/SectionFeature.vue'
+
 
 export default {
   name: 'HomeView',
-  components: { Navbar, Header, SectionAbout },
+  components: { Navbar, Header, SectionAbout, SectionFeature },
   setup() {
     return {}
   }
