ca62f733154bea59a3dcbf5359a99ce058c43024
Mel <meljohnpualon@gmail.com>
2023-05-10 16:43:39 +0800

    added new feature as tab component

--- src/components/Home/SectionFeature.vue
+++ src/components/Home/SectionFeature.vue
@@ -2,14 +2,16 @@
   <div class="container">
     <h2 style="margin-bottom: 1.5rem">FAQ</h2>
     <Accordion />
+    <Tab />
   </div>
 </template>
 
 <script>
 import Accordion from '../Accordion/Accordion.vue'
+import Tab from '../Tabbed/Tab.vue'
 
 export default {
-  components: { Accordion },
+  components: { Accordion, Tab },
   setup() {
   
     return {}
---
+++ src/components/Tabbed/Tab.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-for="tab in tabItems" :key="tab.id">
+    <div class="tabs" @click="selectActiveTab(tab.id)">
+      <h1>{{ tab.tabTitle }}</h1>
+    </div>
+    <div class="tab-content">
+      <p v-if="tab.isActive">
+        {{ tab.tabContent }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  components: {},
+  setup() {
+    const tabItems = ref([
+      { 
+        id: 1,
+        tabTitle: 'Title 1', 
+        tabContent: '1 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, corrupti lorem.',
+        isActive: true
+      },
+      { 
+        id: 2,
+        tabTitle: 'Title 2', 
+        tabContent: '2 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, corrupti. amet consectetur adipisicing elit',
+        isActive: false
+      },
+      { 
+        id: 3,
+        tabTitle: 'Title 3', 
+        tabContent: '3 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, corrupti.',
+        isActive: false
+      }
+    ])
+
+    const selectActiveTab = (id) => {
+      console.log(`selected tab is ${id}`)
+    }
+
+
+    return { tabItems, selectActiveTab }
+  }
+}
+</script>
